c0dafc2aef020c12ee7207522fbc8abe6492e84b
Sangharsh Seth <sethsangharsh@outlook.com>
2024-03-18 19:08:46 +0530

    many changes

--- src/routes/(app)/settings/+layout.svelte
+++ src/routes/(app)/settings/+layout.svelte
@@ -1,15 +1,54 @@
 <script lang="ts">
-  import { IconBrowser, IconDeviceDesktop, IconLockAccess, IconSettings, IconUser, IconWifi } from "@tabler/icons-svelte";
+  import {
+    IconBrowser,
+    IconDeviceDesktop,
+    IconLockAccess,
+    IconSettings,
+    IconUser,
+    IconWifi,
+    IconPremiumRights,
+  } from "@tabler/icons-svelte";
   import type { LayoutData } from "./$types";
   export let data: LayoutData;
   const icons = [
-  { icon:IconUser, name: 'Profile', href: '/settings/profile' },
-  { icon: IconBrowser, name: 'Demo', href: '/settings/demo' },
-  { icon: IconSettings, name: 'Settings', href: '/settings' },
-  { icon: IconWifi, name: 'Network', href: '/settings/network' },
-  { icon: IconLockAccess, name: 'Security', href: '/settings/security' },
-  { icon: IconDeviceDesktop, name: 'Devices', href: '/settings/devices' }
-];
+    {
+      icon: IconUser,
+      name: "Profile",
+      href: "/settings/profile",
+      isSelected: true,
+    },
+    {
+      icon: IconPremiumRights,
+      name: "Membership",
+      href: "/settings/memberships",
+      isSelected: false,
+    },
+
+    {
+      icon: IconWifi,
+      name: "Network",
+      href: "/settings/network",
+      isSelected: false,
+    },
+    {
+      icon: IconLockAccess,
+      name: "Security",
+      href: "/settings/security",
+      isSelected: false,
+    },
+    {
+      icon: IconDeviceDesktop,
+      name: "Devices",
+      href: "/settings/devices",
+      isSelected: false,
+    },
+    {
+      icon: IconSettings,
+      name: "Settings",
+      href: "/settings",
+      isSelected: false,
+    },
+  ];
 </script>
 
 <div class="drawer drawer-open font-Inter">
@@ -21,11 +60,15 @@
     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"
     ></label>
     <ul class="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
-      {#each icons as {icon, href, name} }
-      <div class="flex gap-1 items-center pt-2">
-        <svelte:component this={icon} size={32} stroke={1} />
-        <li class="flex gap"><a href={href}>{name}</a></li>
-      </div>
+      {#each icons as { icon, href, name, isSelected }}
+        <div
+          class={isSelected
+            ? "flex gap-1 bg-slate-900 text-white rounded items-center pt-2"
+            : "flex gap-1 items-center pt-2"}
+        >
+          <svelte:component this={icon} size={32} stroke={1} />
+          <li class="flex gap"><a {href}>{name}</a></li>
+        </div>
       {/each}
     </ul>
   </div>
--- src/routes/(app)/settings/devices/+page.svelte
+++ src/routes/(app)/settings/devices/+page.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
-    async function addDevices(){
-        
-    }
+    let deviceStore = [];
+    async function addDevices(deviceStore: []) {}
 </script>
 
-<div>
-    <button on:click={addDevices} class="btn btn-lg">+</button>
+<div class="w-full h-screen rounded-md">
+    <div class="flex flex-col items-center mt-48 font-Inter">
+        <div
+            class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-3/12 border rounded-md"
+        >
+            <form method="POST">
+                <h1>Add Device Details</h1>
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="iPhone15"
+                    class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none rounded-md"
+                    required
+                />
+
+                <input
+                    id="ip"
+                    type="number"
+                    name="ip"
+                    placeholder="127.0.0.1"
+                    class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none rounded-md"
+                    required
+                />
+                <button
+                    type="submit"
+                    class="w-full py-3 mt-6 font-medium tracking-widest text-white bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none rounded-md"
+                >
+                    Add
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
